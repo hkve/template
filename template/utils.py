7a569241386e7c3562f5cd26068a1597005f6edd
hkve <haakon.kvernmoen@gmail.com>
2022-01-13 11:24:08 +0100

    Fixed docstring for utils

--- template/utils.py
+++ template/utils.py
@@ -1,6 +1,16 @@
 import json
 
 def ask_user(question):
+    """
+    Simple yes/no screen for user, where the "question" string is asked. Takes y(yes)/n(no)
+    as valid inputs. If no valid input is given, the question is asked again.
+
+    Args:
+        question (str): What question should be asked for the y/n menu
+
+    Returns:
+        (bool): true/false, answer to question
+    """
     check = str(input(f"{question} (Y/N): ")).lower().strip()
     try:
         if check[0] in ["y", "yes"]:
@@ -16,5 +26,12 @@ def ask_user(question):
         return ask_user(question)
 
 def write_config(data, path):
+    """
+    Takes a dict data and writes it in json format to path.
+
+    Args:
+        data (dict[str: dict[str: str]]): The data to be written to path
+        path: (PosxPath): Path to the file data will be written to 
+    """
     with open(path, "w+") as file:
         json.dump(data, indent=4, sort_keys=True, fp=file)
